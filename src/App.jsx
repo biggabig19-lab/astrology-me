68f518f59b2ded3e4ecc392149bb7fbe13d0cb1b
biggabig19-lab <biggabig19@gmail.com>
2026-04-18 07:22:24 -0400

    Delete App.jsx

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24, color: 'white', background: '#111827', minHeight: '100vh' }}>
-      <h1>Deployment test works</h1>
-      <p>If you can see this, Vercel is fine and the original App.jsx has a paste/syntax issue.</p>
-    </div>
-  );
-}
